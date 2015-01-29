e4016fa2282c0de20ee5e0a0f62dad616ed3833a
di wu <di.wu@me.com>
2015-01-29 22:02:04 +0800

    first commit

---
+++ is.playground/section-1.swift
@@ -0,0 +1,66 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+class _is {
+    
+    class func not(){
+    
+    }
+    
+    class func all(){
+        
+    }
+    
+    class func any(){
+        
+    }
+    
+//TYPE CHECK
+    class func arguments() {
+    
+    }
+    
+    class func array(value: AnyObject) -> Bool{
+        return value is Array
+    }
+    
+    class func bool(value: AnyObject) -> Bool{
+        return value is Bool
+    }
+    
+    class func date(value: AnyObject) -> Bool{
+        
+    }
+    
+    class func error() {
+        
+    }
+    
+    class func null() {
+        
+    }
+    
+    class func number() {
+    
+    }
+    
+    class func object() {
+    
+    }
+    
+    class func regexp() {
+    
+    }
+    
+    class func string(value: AnyObject) -> Bool{
+        return value is String
+    }
+}
+
+
+
+_is.string(1)
+
