1bac692fdefcd825f3b2872dca18e09e63109f3f
di wu <di.wu@me.com>
2015-02-14 21:25:13 +0800

    add all the empty func

--- is.playground/section-1.swift
+++ is.playground/section-1.swift
@@ -7,15 +7,6 @@ var str = "Hello, playground"
 
 public class _is {
     
-    typealias returnedFunctionType = (AnyObject) -> String
-    
-
-    class func luckyNumberForName(#lotteryHandler: (Int) -> String) -> String {
-        let luckyNumber = Int(arc4random() % 100)
-        return lotteryHandler(luckyNumber)
-    }
-
-    
     class func not(){
         
     }
@@ -73,7 +64,148 @@ public class _is {
         return value is String
     }
     
+    class func chat(value:AnyObject) -> Bool {
+        return false
+    }
+    
+    class func empty(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func existy(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func truthy(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func falsy(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func space(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func even(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func odd(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func positive(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func negative(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func above(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func under(value: AnyObject) -> Bool{
+        return false
+    }
+
+    
+    class func within(value: AnyObject, min:AnyObject, max:AnyObject) -> Bool{
+        return false
+    }
+    
+    class func decimal(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func integer(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func finite(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func infinite(value: AnyObject) -> Bool{
+        return false
+    }
+    
+//STRING CHECK
+    class func include(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func upperCase(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func lowerCase(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func startWith(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func endWith(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func capitalized(value: AnyObject) -> Bool{
+        return false
+    }
+    
+    class func palindrome(value: AnyObject) -> Bool{
+        return false
+    }
 //date
+    class func today() {
+    
+    }
+    
+    class func tomorrow() {
+        
+    }
+    
+    class func yesterday() {
+        
+    }
+    
+    class func past() {
+        
+    }
+    
+    class func future() {
+        
+    }
+    
+    class func day() {
+        
+    }
+    
+    class func month() {
+        
+    }
+    
+    class func year() {
+        
+    }
+    
+    class func leapYear() {
+        
+    }
+    
+    class func weekend() {
+        
+    }
+    
+    class func weekday() {
+        
+    }
+
     class func inDateRange(give: AnyObject,start: AnyObject, end: AnyObject) -> Bool{
         if (_is.not().date(give
             ) || _is.not().date(start) || _is.not().date(end)){
